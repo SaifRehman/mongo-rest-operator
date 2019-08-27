f05e7c88578a8452559fc0f119011911f90f6181
saif <s4saif.121@gmail.com>
2019-08-27 11:17:35 +0400

    added

--- src/controllers/listing/lib/models/crmModel.ts
+++ src/controllers/listing/lib/models/crmModel.ts
@@ -1,20 +1,6 @@
 import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-const config = require(process.env.PATHOF);
+const config = require(process.env.DATAMODEL);
 
-export const TodoSchema = new Schema({
-    "Username": {
-        "type": "String"
-    },
-    "Task": {
-        "type": "String"
-    },
-    "Completed":{
-        "type": "Boolean"
-    },
-    "Time": {
-        "type": "String"
-    }
-    
-});
+export const TodoSchema = new Schema(JSON.parse(config));

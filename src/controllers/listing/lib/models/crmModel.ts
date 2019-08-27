fe1532092df98a413764fb6da7c0640c12603c77
saif <s4saif.121@gmail.com>
2019-08-27 11:52:58 +0400

    added

--- src/controllers/listing/lib/models/crmModel.ts
+++ src/controllers/listing/lib/models/crmModel.ts
@@ -3,4 +3,4 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 const config = process.env.MODEL;
 
-export const TodoSchema = new Schema(JSON.parse(config));
+export const TodoSchema = new Schema(JSON.parse(config.replace(/'/g,"\"").replace(/"s/g,"'")));

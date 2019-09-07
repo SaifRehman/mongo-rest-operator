7b3589c219f27831d38ca81b3096f6e327bac030
saif <s4saif.121@gmail.com>
2019-09-07 21:59:19 +0400

    ref

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,12 @@ const collectDefaultMetrics = client.collectDefaultMetrics;
 
 // define a custom prefix string for application metrics
 collectDefaultMetrics({ prefix: "my_application:" });
+const histogram = new client.Histogram({
+  name: 'my_application:hello_duration',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'status_code'],
+  buckets: [0.1, 5, 15, 50, 100, 500]
+});
 mongoose.Promise = Promise;
 class App {
   public express: express.Application;
@@ -58,6 +64,15 @@ class App {
   private routes(): void {
     this.express.get("/", function(_, res) {
       res.send("hi");
+    });
+    this.express.get("/greetings", function(request, res) {
+        // start the timer for our custom metric - this returns a function
+  // called later to stop the timer
+  const end = histogram.startTimer();
+  const name = request.query.name ? request.query.name : 'World';
+  res.send({content: `Hello, ${name}!`});
+  // stop the timer
+  end({ method: request.method, 'status_code': 200 });
     });
     this.express.get("/metrics", function(_, response) {
       response.set("Content-Type", client.register.contentType);
